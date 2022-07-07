f251b86bbe1b2ee8dd9c4f45a35ccf759cc41339
fyauriba <fyauriba@everis.com>
2022-07-07 09:35:42 -0500

    first commit

---
+++ src/main/java/com/proyecto/msreport/MsReportApplication.java
@@ -0,0 +1,13 @@
+package com.proyecto.msreport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsReportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsReportApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/proyecto/msreport/MsReportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyecto.msreport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsReportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
